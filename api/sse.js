5f2d13bbcafbdb14de17d6bb3ae97e1d15d13c41
arpan1235 <arpangrover1904@gmail.com>
2025-05-29 17:02:14 -0400

    Update sse.js

--- api/sse.js
+++ api/sse.js
@@ -31,7 +31,4 @@ export default async function handler(req, res) {
     res.setHeader("Allow", "GET,POST");
     res.statusCode = 405; res.end();
   }
-}
-
-// Ensure Node.js runtime for streaming
-export const config = { runtime: 'nodejs' }; 
+} 
